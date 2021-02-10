f0090dd6ecf53d41566a69c68b73c0e8a70810d9
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-02-10 11:36:31 -0300

    Removidas funcoes depreciadas.

--- src/modelo/dao/ProdutoDAO.java
+++ src/modelo/dao/ProdutoDAO.java
@@ -3,7 +3,6 @@ package modelo.dao;
 import conexao.ConexaoFactory;
 import modelo.bean.Produto;
 
-import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,15 +10,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * <p> Gerencia todas as operacoes no banco de dados que envolvam a tabela de
  * produtos. </p>
  *
  * @author Matheus
- * @version 1.1.0
+ * @version 2.0.0
  * @since 1.0.0
  */
 public class ProdutoDAO implements DAO<Produto> {
@@ -171,114 +168,4 @@ public class ProdutoDAO implements DAO<Produto> {
             ConexaoFactory.fecharConexao(connection, statement);
         }
     }
-
-
-    // --- Sera removido logo
-    @Deprecated
-    public void create(Produto p) {      //CRUD=CREATE,UPDATE E DELETE
-
-        Connection        con  = ConexaoFactory.getConnection();        //abrindo a conexao
-        PreparedStatement stmt = null;                            //preparando e executando
-
-        try {
-            stmt = con.prepareStatement("INSERT INTO tb_produto (nome,tipo,descricao,quantidade,preco) VALUES(?,?,?,?,?)");
-            stmt.setString(1, p.getNome());
-            stmt.setString(2, p.getTipo());
-            stmt.setString(3, p.getDescricao());
-            stmt.setInt(4, p.getQuantidade());
-            stmt.setDouble(5, p.getPreco());
-
-            stmt.executeUpdate();                             //executando a sql
-
-            JOptionPane.showMessageDialog(null, "Salvo com Sucesso!");     //exibindo mensagem se deu certo a inserçao
-
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Erro ao salvar!" + ex);
-        } finally {
-            ConexaoFactory.closeConnection(con, stmt);
-        }
-    }
-
-    @Deprecated
-    public List<Produto> read() {                              //retornar os dados do banco e inserir no jTable usando uma lista
-
-        Connection        con  = ConexaoFactory.getConnection();        //abrindo a conexao
-        PreparedStatement stmt = null;
-        ResultSet         rs   = null;
-
-        List<Produto> produtos = new ArrayList<>();   //colocando dentro de uma lista
-
-        try {
-            stmt = con.prepareStatement("SELECT * FROM tb_produto");   //selecionando todos os campos da tabela mercado
-            rs   = stmt.executeQuery();                                 //colocando os valores dentro do "rs" que é o ResultSet
-
-            while (rs.next()) {           //quando existir um valor ele passara para o proximo e vai colocar o objeto, e vai indo ate retornar false
-
-                Produto produto = new Produto();            //chamando o objeto
-
-                produto.setId(rs.getInt("id"));
-                produto.setNome(rs.getString("nome"));
-                produto.setTipo(rs.getString("tipo"));
-                produto.setDescricao(rs.getString("descricao"));
-                produto.setQuantidade(rs.getInt("quantidade"));     //colocando os valores dentro do objeto
-                produto.setPreco(rs.getDouble("preco"));
-                produtos.add(produto);
-            }
-
-        } catch (SQLException ex) {
-            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {   //chamar a classe conection e pegar a ultima sobrecarga
-            ConexaoFactory.closeConnection(con, stmt, rs);
-        }
-
-        return produtos;
-    }
-
-    @Deprecated
-    public void update(Produto p) {
-
-        Connection        con  = ConexaoFactory.getConnection();
-        PreparedStatement stmt = null;
-
-        try {
-            stmt = con.prepareStatement("UPDATE tb_produto SET nome = ?, tipo = ?, descricao = ?, quantidade = ?, preco = ? WHERE id = ?");
-            stmt.setString(1, p.getNome());
-            stmt.setString(2, p.getTipo());
-            stmt.setString(3, p.getDescricao());
-            stmt.setInt(4, p.getQuantidade());
-            stmt.setDouble(5, p.getPreco());
-            stmt.setInt(6, p.getId());
-
-            stmt.executeUpdate();                             //executando a sql
-
-            JOptionPane.showMessageDialog(null, "Atualizado com sucesso!");     //exibindo mensagem se deu certo a inserçao
-
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Erro ao atualizar!" + ex);
-        } finally {
-            ConexaoFactory.closeConnection(con, stmt);
-        }
-    }
-
-    @Deprecated
-    public void Delete(Produto p) {
-
-        Connection        con  = ConexaoFactory.getConnection();
-        PreparedStatement stmt = null;
-
-        try {
-            stmt = con.prepareStatement("DELETE FROM tb_produto WHERE id = ?");
-            stmt.setInt(1, p.getId());
-
-            stmt.executeUpdate();                             //executando a sql
-
-            JOptionPane.showMessageDialog(null, "Deletado com Sucesso!");     //exibindo mensagem se deu certo a inserçao
-
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Erro ao deletar!" + ex);
-        } finally {
-            ConexaoFactory.closeConnection(con, stmt);
-        }
-    }
-    // --- Sera removido logo
 }
