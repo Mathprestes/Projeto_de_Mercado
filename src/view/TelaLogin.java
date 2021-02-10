233d68dacf78fe134d36fe8b2d36d7328acf2553
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-02-10 11:26:33 -0300

    Usa a imagem com tamanho padrao.

--- src/view/TelaLogin.java
+++ src/view/TelaLogin.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
 import modelo.dao.UsuarioDAO;
-import java.awt.Color;
-import javax.swing.BorderFactory;
-import javax.swing.JOptionPane;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  *
